915f92c5e5d3effdaf726fa426179ad6899e95a7
Sanket123patil <patilsanket7744@gmail.com>
2025-09-04 17:43:51 +0530

    Fix prcing

--- src/landing_page/home/HomePage.js
+++ src/landing_page/home/HomePage.js
@@ -1,28 +1,27 @@
-import React from 'react'
-import Hero from './Hero' 
-import Awards from './Awards';
+import React from "react";
+import Hero from "./Hero";
+import Awards from "./Awards";
 
-import Education from './Education';
-import OpenAccount from '../OpenAccount';
+import Pricing from "./Pricing";
+import Education from "./Education";
 
-import Statss from './Statss';
-import Pricing from './Pricing';
+import OpenAccount from "../OpenAccount";
 
+import Statss from "./Statss";
 
 function HomePage() {
-    return ( 
-      <>
+  return (
+    <>
      
-      <Hero/>
-      <Awards/>
-      <Statss/>
-      <Pricing/>
-      <Education/>
-      <OpenAccount/>
-      
-
-      </>
-     );
+      <Hero />
+      <Awards />
+      <Statss />
+      <Pricing />
+      <Education />
+      <OpenAccount />
+    
+    </>
+  );
 }
 
-export default HomePage ;
+export default HomePage;
