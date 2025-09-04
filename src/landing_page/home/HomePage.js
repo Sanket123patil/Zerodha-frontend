ea1b013514f1edf69360321ebf66729d7fcf615d
Sanket123patil <patilsanket7744@gmail.com>
2025-09-04 17:23:20 +0530

    Fix prcing

--- src/landing_page/home/HomePage.js
+++ src/landing_page/home/HomePage.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Hero from './Hero' 
 import Awards from './Awards';
-import Pricing from './Pricing';
+
 import Education from './Education';
 import OpenAccount from '../OpenAccount';
 import Stats from './Stats';
+import Pricing from './Pricing';
 
 
 function HomePage() {
