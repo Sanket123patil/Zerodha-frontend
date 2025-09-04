8b24ee6007822398c1b8490f02d709c4a84545c6
Sanket123patil <patilsanket7744@gmail.com>
2025-09-04 17:18:39 +0530

    Fix Stats.js import path case sensitivity

--- src/landing_page/home/HomePage.js
+++ src/landing_page/home/HomePage.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import Hero from './Hero' 
 import Awards from './Awards';
-import Stats from './Stats'
 import Pricing from './Pricing';
 import Education from './Education';
 import OpenAccount from '../OpenAccount';
+import Stats from './Stats';
 
 
 function HomePage() {
